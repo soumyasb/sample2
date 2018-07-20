7d447d3d6913e958efc22f3d164929405b47889f
soumyasb <40511542+soumyasb@users.noreply.github.com>
2018-07-20 11:05:51 +0530

    Add files via upload

--- krishna/CaseDetails.js
+++ krishna/CaseDetails.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import { searchCases } from '../../../../../store/actions/caseActions';
-import { Card, Row, Col, Button, Icon, Menu } from 'antd';
+import { Card, Row, Col, Button, Icon, Menu, Input, Form, Select } from 'antd';
 import MenuItem from "antd/lib/menu/MenuItem";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+
+const FormItem = Form.Item;
+const { Option } = Select;
 
 class CaseDetails extends Component {
     constructor(props) {
         super(props);
 
         this.state={
-            // whatever the props needed
+          editMode: false,
         };
+
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleSave = this.handleSave.bind(this);
     }    
+
+    handleEdit() {
+      this.setState({ editMode: true });
+    }
+
+    handleSave() {
+      this.setState({ editMode: false });
+
+      // API call to save the Case....
+    }
 
     render() {
       const boxShadows = {
         boxShadow: 
           "0 1px 3px 0 rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.12), 0 0 2px 0 rgba(0, 0, 0, 0.14)"
       };
+
+      const { editMode } = this.state;
 
       return (
         <div>
@@ -78,20 +96,97 @@ class CaseDetails extends Component {
                           <h2><Icon type="profile" />&nbsp;Case Detail</h2>
                         </div>
                         <div style={{ flex: 1 }}>
-                          <Button style={{ margin: '5px' }}><Icon type="edit" />Edit</Button>
+                          <Button style={{ margin: '5px' }} onClick={this.handleEdit} disabled={editMode}><Icon type="edit" />Edit</Button>
                           <Button style={{ margin: '5px' }}><Icon type="delete" />Delete</Button>
-                          <Button type="primary" disabled={true} style={{ margin: '5px' }}><Icon type="save" />Save</Button>
+                          <Button type="primary" disabled={!editMode} onClick={this.handleSave} style={{ margin: '5px' }}><Icon type="save" />Save</Button>
                         </div>
                       </div>}
                       bodyStyle={{
                         padding: "5px 30px"
                       }}
                     >
-                      <div style={{display: 'flex' }}>
-                        <div>
+                      <Form className="ant-advanced-search-form">
+                        <Col span={7}>
+                          <Row gutter={12}>
+                            <FormItem label={`Date of Birth`}>
+                              <Input placeholder="Date of Birth" disabled={!editMode}/>
+                            </FormItem>
+                          </Row>
+                          <Row gutter={12}>
+                            <FormItem label={`DL Number`}>
+                              <Input placeholder="DL Number" disabled={!editMode}/>
+                            </FormItem>
+                          </Row>
+                          <Row gutter={12}>
+                            <FormItem label={`License Class`}>
+                              <Input placeholder="License Class" disabled={!editMode}/>
+                            </FormItem>
+                          </Row>
+                          <Row gutter={12}>
+                            <FormItem label={`Phone Number`}>
+                              <Input placeholder="Phone Number" disabled={!editMode}/>
+                            </FormItem>
+                          </Row>
+                          <Row gutter={12}>
+                            <FormItem label={`Mailing Address`}>
+                              <Input.TextArea placeholder="Mailing Address" rows="6" disabled={!editMode} />
+                            </FormItem>
+                          </Row>
+                        </Col>
+                        <Col span={1} />
+                        <Col span={7}>
+                          <Row gutter={12}>
+                            <FormItem label={`Case Number`}>
+                              <Input placeholder="Case Number" disabled={!editMode} />
+                            </FormItem>
+                          </Row>
+                          <Row gutter={12}>
+                            <FormItem label={`Received Date`}>
+                              <Input placeholder="Received Date" disabled={!editMode}/>
+                            </FormItem>
+                          </Row>
+                          <Row gutter={12}>
+                            <FormItem label={`Case Status`}>
+                              <Input placeholder="Case Status" disabled={!editMode}/>
+                            </FormItem>
+                          </Row>
+                          <Row gutter={12}>
+                            <FormItem label={`Hearing Type`}>
+                              <Select placeholder='Hearing Type' disabled={!editMode}>
+                                <Option value="0">0 - Telephone interview</Option>
+                                <Option value="1">1 - Departmental Review</Option>
+                                <Option value="2">2 - Interview</Option>
+                                <Option value="3">3 - Re-examination</Option>
+                                <Option value="4">4 - Hearing</Option>
+                              </Select>
+                            </FormItem>
+                          </Row>
+                          <Row gutter={12}>
+                            <FormItem label={`Reason`}>
+                              <Input placeholder="Reason" disabled={!editMode}/>
+                            </FormItem>
+                          </Row>
+                          <Row gutter={12}>
+                            <FormItem label={`Referral`}>
+                              <Input placeholder="Referral" disabled={!editMode}/>
+                            </FormItem>
+                          </Row>
+                          <Row gutter={12}>
+                            <FormItem label={`Special Cert/Endorsement`}>
+                              <Input placeholder="Special Cert/Endorsement" disabled={!editMode}/>
+                            </FormItem>
+                          </Row>
+                        </Col>
+                        <Col span={1} />
+                        <Col span={7}>
+                        </Col>
+                      </Form>
+
+                      {/* <div style={{display: 'flex' }}>
+                        <div style={{ flex: 1 }}>
                           <div>
                               DOB
-                              Input
+                              <Input />
                           </div>
                           <div>
                               DL Number
@@ -106,9 +201,13 @@ class CaseDetails extends Component {
                               Input
                           </div>
                         </div>
-                        <div>
+                        <div style={{ flex: 1 }}>
+                          <div>
+                              Case Number
+                              Input
+                          </div>
                         </div>
-                      </div>
+                      </div> */}
                     </Card>
                   </Col>
                 </Row>
