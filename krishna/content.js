d1f2bac97c4d1d2774201460205a7ebfb5f7598b
soumyasb <40511542+soumyasb@users.noreply.github.com>
2018-07-25 11:37:21 +0530

    Sched and Unsched Cases

--- krishna/content.js
+++ krishna/content.js
@@ -4,6 +4,10 @@ import { Switch, Route } from 'react-router-dom'
 import HomePage from "./contents/home/homepage";
 import NewsItemPage from "./contents/admin/News/NewsItemPage";
 import Search from "./Search";
+import CustomerDetails from './contents/admin/Cases/case/CustomerDetails';
+import CaseDetails from './contents/admin/Cases/case/CaseDetails';
+import UnscheduledCases from './contents/admin/Cases/case/UnscheduledCases';
+import ScheduledCases from './contents/admin/Cases/case/ScheduledCases';
 
 const content = () => (
   <div className="contentlayout">
@@ -11,6 +15,10 @@ const content = () => (
       <Route exact path="/" component={HomePage} />
       <Route exact path="/news" component={NewsItemPage} />
       <Route path="/search/searchText/:searchText" component={Search} />
+      <Route path="/customerdetails" component={CustomerDetails} />
+      <Route path="/casedetails" component={CaseDetails} />
+      <Route path="/scheduledCases" component={ScheduledCases} />
+      <Route path="/UnscheduledCases" component={UnscheduledCases} />
     </Switch>
   </div>
 );
